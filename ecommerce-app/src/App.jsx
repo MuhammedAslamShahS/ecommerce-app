839e819b83fd3372af931809c744ee961420beb9
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-03-18 15:14:11 +0530

    initial commit : {header make}

---
+++ ecommerce-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+const App = () => {
+    return (
+        <div style={{
+            padding: "10px 100px"
+        }}>
+            <Header />
+        </div>
+    );
+};
+
+export default App;
---
+++ ecommerce-app/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='header-h1'>Store</h1>
+      <button className='logout-btn'>Log Out</button>
+    </header>
+  )
+}
+
+export default Header
