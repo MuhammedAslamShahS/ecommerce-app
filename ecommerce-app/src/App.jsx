41c73168ccbd9c25f32a51e907ba135d92b1cebb
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-03-18 15:33:56 +0530

    2nd commit : {make the header to responsive with media query}

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return (
         <div style={{
-            padding: "10px 100px"
+            padding: "10px 30px"
         }}>
             <Header />
+            <Footer/>
         </div>
     );
 };
---
+++ ecommerce-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.jsx";
+
+const Footer = () => {
+    return <div className="footer">&copy; Store {new Date().getFullYear} Footer</div>;
+};
+
+export default Footer;
