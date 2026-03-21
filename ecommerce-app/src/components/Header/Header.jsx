9f494ccc379941ef31e37144325626387cea79cb
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-03-21 18:17:02 +0530

    updated

--- ecommerce-app/src/components/Header/Header.jsx
+++ ecommerce-app/src/components/Header/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import { IoIosSearch } from "react-icons/io";
+import { CiDeliveryTruck } from "react-icons/ci";
+
+import HeaderTopBar from "./HeaderTopBar";
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <>
+            <HeaderTopBar />
 
-export default Header
+            <div className="navbar">
+                {/* Logo */}
+                <img src={logo} alt="" className="logo" />
+
+                {/* Navlinks */}
+                <ul>
+                    <li>NEW IN</li>
+                    <li>SALES</li>
+                    <li>PRODUCTS</li>
+                    <li>COLLECTIONS</li>
+                    <li>WEDDING</li>
+                    <li>DEALS</li>
+                </ul>
+
+                {/* Search Bar */}
+                <div className="search-box">
+                    <input type="text" placeholder="Search" />
+                    <IoIosSearch className="search-icon" />
+                </div>
+
+                <div className="Express-delivery-container">
+                  <CiDeliveryTruck className="Experss-truck" />
+                  <div className="express-text-container">
+                    <p className="express-text-1">EXPRESS DELIVERY</p>
+                    <p className="express-text-2">Upgrade Your Delivery Method</p>
+                  </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
---
+++ ecommerce-app/src/components/Header/HeaderTopBar.jsx
@@ -0,0 +1,66 @@
+import "./HeaderTopBar.css";
+import { LuMapPin } from "react-icons/lu";
+import { BiMessageError } from "react-icons/bi";
+import { RiAccountCircleFill } from "react-icons/ri";
+import { CiHeart } from "react-icons/ci";
+import { AiOutlineShopping } from "react-icons/ai";
+
+const HeaderTopBar = () => {
+    return (
+        <>
+            <div className="topbar-container-1">
+                <p>All discounts are inclusive of GST rate cut benefits.</p>
+                <p>
+                    Download the Store App and get additional 15% discount on your first purchase. Use code:
+                    STOREAPP777 *T&C Apply
+                </p>
+            </div>
+
+            {/* ================================================== */}
+
+            <div className="topbar-container-2">
+                {/*  section-1*/}
+
+                <div className="about">ABOUT US</div>
+
+                {/* section-2 */}
+                <div className="delivery-store-container">
+                    <div className="delivery">
+                        <p className="delivery-text-1">Delivering to</p>
+                        <p className="delivery-text-2">Add delivery location</p>
+                    </div>
+                    <div className="store">
+                        <LuMapPin className="map-icon icon" />
+                        <p className="store-text">STORES</p>
+                    </div>
+                </div>
+
+                {/* section-3 */}
+
+                <div className="section-3">
+                    <div className="contact-container">
+                        <p className="contact-text">CONTACT US</p>
+                        <BiMessageError className="message-icon icon " />
+                    </div>
+
+                    <div className="my-profile-container">
+                        <RiAccountCircleFill className="profile-icon icon" />
+                        <p className="profile-text">MY ACCOUNT</p>
+                    </div>
+
+                    <div className="love-cart-container">
+                        <div className="love-container">
+                            <CiHeart className="love-icon icon" />
+                        </div>
+
+                        <div className="cart-container">
+                            <AiOutlineShopping className="cart-icon icon" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default HeaderTopBar;
